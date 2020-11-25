aeda80b9b704ce1495f0791559281f59099531c3
Tomas Matusek <tmatusek@datto.com>
2020-11-25 22:20:02 +0100

    Added templates for section: Activity, Contact, Login

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -7,9 +7,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 import { FooterComponent } from './footer/footer.component';
+import { ActivityComponent } from './activity/activity.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: AboutComponent }
+  { path: '', component: AboutComponent },
+  { path: 'activity', component: ActivityComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
@@ -17,7 +23,10 @@ const routes: Routes = [
     AppComponent,
     AboutComponent,
     SidemenuComponent,
-    FooterComponent
+    FooterComponent,
+    ActivityComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +37,6 @@ const routes: Routes = [
   exports: [
     FooterComponent,
     AboutComponent,
-    SidemenuComponent,
     SidemenuComponent
   ],
   bootstrap: [AppComponent],
