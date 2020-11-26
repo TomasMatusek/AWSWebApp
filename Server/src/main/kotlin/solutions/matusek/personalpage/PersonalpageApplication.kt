e44a90768543e13c516ba3368c74dc6331e6e565
Tomas Matusek <tmatusek@datto.com>
2020-11-26 08:45:00 +0100

    Project recreated using Kotlin 1.4 and Java 11

---
+++ Server/src/main/kotlin/solutions/matusek/personalpage/PersonalpageApplication.kt
@@ -0,0 +1,11 @@
+package solutions.matusek.personalpage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PersonalpageApplication
+
+fun main(args: Array<String>) {
+	runApplication<PersonalpageApplication>(*args)
+}
